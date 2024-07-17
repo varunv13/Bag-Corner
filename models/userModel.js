516b50a936682fa1b601424ecc04311e3ad5232f
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-07-17 16:18:57 +0530

    added seperation of concerns in models and created owner model with respective model's routes

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
 
 const userSchema = mongoose.Schema({
-    fullname: String,
+    fullname: {
+        type: String,
+        minLength: 3,
+        trime: true,
+    },
     email: String,
     password: String,
     contact: Number,
